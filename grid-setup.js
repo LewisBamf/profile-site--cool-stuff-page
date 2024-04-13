6a051c9191a2729d39bd6a70be95aff6bce70a80
LewisBamf <bamfordlewis9@gmail.com>
2024-04-13 09:58:46 +0100

    fixed js positioning

--- grid-setup.js
+++ grid-setup.js
@@ -1,16 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){
+
 const NAV = document.getElementById("navbar");
 
 let NAVheight = NAV.clientHeight;
+console.log(NAVheight)
 
 const grid = document.getElementById("page-layout");
 
 
 
-grid.style.transform = "translate(0px, " + -NAVheight + "px)";
+grid.style.transform = "translate(0px, " + NAVheight + "px)";
 
 window.onresize = function(){
-    
-    NAVheight = NAV.clientHeight;
 
-    grid.style.transform = "translate(0px, " + -NAVheight + "px)";
-}
+    NAVheight = NAV.clientHeight;
+    console.log(NAVheight)
+
+    grid.style.transform = "translate(0px, " + NAVheight + "px)";
+};
+
+});
